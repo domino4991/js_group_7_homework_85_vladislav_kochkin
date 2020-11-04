75ad6b6c69592e2bf941e1552c1518b7ec486093
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-11-04 15:33:20 +0600

    Добавил роуты юзеров и истории треков в главный файл

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const artists = require('./app/artists');
 const albums = require('./app/albums');
 const tracks = require('./app/tracks');
+const users = require('./app/users');
+const trackHistory = require('./app/track_history');
 
 const app = express();
 const PORT = 8000;
@@ -13,13 +15,19 @@ app.use(cors());
 app.use(express.static('public'));
 
 const run = async () => {
-    await mongoose.connect("mongodb://localhost/musicApp", {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect("mongodb://localhost/musicApp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
 
     console.log("Connected to MongoDB");
 
     app.use('/artists', artists());
     app.use('/albums', albums());
     app.use('/tracks', tracks());
+    app.use('/users', users);
+    app.use('/track_history', trackHistory);
 
     app.listen(PORT, () => {
         console.log(`Server started at http://localhost:${PORT}`);

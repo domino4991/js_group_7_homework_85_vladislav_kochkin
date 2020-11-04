3e6a907c272ef01f4a84a7adb2c3d66f68185c07
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-11-04 15:32:36 +0600

    Сделал роут для истории треков

---
+++ app/track_history.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const TrackHistory = require('../models/TrackHistory');
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+    const token = req.get('Authorization');
+    if(!token) return res.status(401).send({error: 'No token present'});
+    try {
+        const user = await User.findOne({token});
+        if(!user) return res.status(401).send({error: 'Wrong token!'});
+        const trackHistory = new TrackHistory(req.body);
+        await trackHistory.save();
+        return res.send(trackHistory);
+    } catch (e) {
+        return res.status(400).send({error: 'Bad request'});
+    }
+});
+
+module.exports = router;

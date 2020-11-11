421fa97b3dccd5b5aae500de80575e96836ce926
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-11-11 17:54:58 +0600

    Сделал роуты

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import './App.css';
 import ArtistsPage from "./containers/ArtistsPage/ArtistsPage";
+import Layout from "./components/Layout/Layout";
+import {Route, Switch} from "react-router-dom";
+import AlbumsPage from "./containers/AlbumsPage/AlbumsPage";
+import TracksPage from "./containers/TracksPage/TracksPage";
 
 const App = () => {
   return (
-    <div className="App">
-      <ArtistsPage />
-    </div>
+    <Layout>
+      <Switch>
+          <Route path="/" exact component={ArtistsPage} />
+          <Route path="/albums" exact component={AlbumsPage} />
+          <Route path="/tracks" exact component={TracksPage} />
+          <Route render={() => <h1 style={{textAlign: 'center'}}>404 not found</h1>} />
+      </Switch>
+    </Layout>
   );
 }
 

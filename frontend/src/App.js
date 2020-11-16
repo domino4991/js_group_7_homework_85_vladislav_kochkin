b0c57debfe8d83983c48b58baa553a4b285497b8
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-11-16 13:49:16 +0600

    HW#86 - Добавил роуты для страниц рег/лог и истории

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Layout from "./components/Layout/Layout";
 import {Route, Switch} from "react-router-dom";
 import AlbumsPage from "./containers/AlbumsPage/AlbumsPage";
 import TracksPage from "./containers/TracksPage/TracksPage";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import TrackHistory from "./containers/TrackHistory/TrackHistory";
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
           <Route path="/" exact component={ArtistsPage} />
           <Route path="/albums" exact component={AlbumsPage} />
           <Route path="/tracks" exact component={TracksPage} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/track_history" exact component={TrackHistory} />
           <Route render={() => <h1 style={{textAlign: 'center'}}>404 not found</h1>} />
       </Switch>
     </Layout>

e7d8abde793997cc8c24953b640b3a8a68e91692
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-11-17 16:44:30 +0600

    HW#87 - Добавил в конфиг файл путь до аудио и вынес конфиг подключения к БД

--- backend/config.js
+++ backend/config.js
@@ -4,5 +4,12 @@ const rootPath = __dirname;
 
 module.exports = {
     rootPath,
-    uploadPath: path.join(rootPath, 'public/uploads')
-}
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    audioUploadPath: path.join(rootPath, 'public/uploads/audio'),
+    database: "mongodb://localhost/musicApp",
+    databaseOpt: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
+};
